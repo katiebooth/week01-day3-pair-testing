5f11358c5f61c80202bf784a2cb410762dc08f35
Stephen Kinnaird <slkinnaird@gmail.com>
2022-07-27 14:52:15 +0100

    the updates

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -18,3 +18,40 @@ def length_of_string(string):
 
 def join_string(string1, string2):
     return string1 + string2
+
+def add_string_as_number(string1, string2):
+    return int(string1) + int(string2)
+
+def number_to_full_month_name(month_number):
+    month_dict = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December",
+    }
+    return month_dict[month_number]
+
+def number_to_short_month_name(month_number):
+    month_short_dict = {
+        1: "Jan",
+        2: "Feb",
+        3: "Mar",
+        4: "Apr",
+        5: "May",
+        6: "Jun",
+        7: "Jul",
+        8: "Aug",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dece",
+    }
+    return month_short_dict[month_number]
--- tests/python_functions_test.py
+++ tests/python_functions_test.py
@@ -36,37 +36,30 @@ class TestPythonFunctionPractice(unittest.TestCase):
       joined_string = join_string( string_1, string_2 )
       self.assertEqual( "Mary had a little lamb, its fleece was white as snow", joined_string )
 
-  @unittest.skip("delete this line to run the test")
   def test_add_string_as_number(self):
       add_result = add_string_as_number( "1", "2" )
       self.assertEqual( 3, add_result )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_full_name__month_1(self):
       result = number_to_full_month_name( 1 )
       self.assertEqual( "January", result )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_full_name__month_3(self):
       result = number_to_full_month_name( 3 )
       self.assertEqual( "March", result )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_full_name__month_9(self):
       result = number_to_full_month_name( 9 )
       self.assertEqual( "September", result )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_short_month_name__month_1(self):
       first_month_string = number_to_short_month_name( 1 )
       self.assertEqual( "Jan", first_month_string )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_short_month_name__month_4(self):
       fourth_month_string = number_to_short_month_name( 4 )
       self.assertEqual( "Apr", fourth_month_string )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_short_month_name__month_10(self):
       tenth_month_string = number_to_short_month_name( 10 )
       self.assertEqual( "Oct", tenth_month_string )

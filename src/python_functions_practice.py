d2247202608df8c335ec191f0f8934000320d9ce
Stephen Kinnaird <slkinnaird@gmail.com>
2022-07-27 14:30:52 +0100

    getting there

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -6,3 +6,15 @@ def add(num1, num2):
 
 def subtract(num1, num2):
     return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def length_of_string(string):
+    return len(string)
+
+def join_string(string1, string2):
+    return string1 + string2
--- tests/python_functions_test.py
+++ tests/python_functions_test.py
@@ -17,23 +17,19 @@ class TestPythonFunctionPractice(unittest.TestCase):
       subtract_result = subtract( 10, 5 )
       self.assertEqual( 5, subtract_result )
 
-  @unittest.skip("delete this line to run the test")
   def test_multiply(self):
       multiply_result = multiply( 4, 2 )
       self.assertEqual( 8, multiply_result )
 
-  @unittest.skip("delete this line to run the test")
   def test_divide(self):
       divide_result = divide( 10, 2 )
       self.assertEqual( 5, divide_result )
 
-  @unittest.skip("delete this line to run the test")
   def test_length_of_string(self):
       test_string = "A string of length 21"
       string_length = length_of_string( test_string )
       self.assertEqual( 21, string_length )
 
-  @unittest.skip("delete this line to run the test")
   def test_join_string(self):
       string_1 = "Mary had a little lamb, "
       string_2 = "its fleece was white as snow"
